b16eaa34370845abff9a60865cb57a4cb18fbe80
Denilson Rosestolato <denilsonprofissional2332@gmail.com>
2024-06-02 00:18:11 -0300

    Seller details and eager loading

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using Sales_Web_MVC.Data;
 using Sales_Web_MVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Sales_Web_MVC.Services
 {
@@ -24,7 +25,7 @@ namespace Sales_Web_MVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj=> obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int  id) 
